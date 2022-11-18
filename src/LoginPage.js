6edd651b4b34d8b9ea4a00b0e6ef53f2988525c2
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-18 12:41:01 -0300

    add form in login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,17 +1,36 @@
 import styled from "styled-components";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   return (
     <Container>
-      <h1>Hello everyone</h1>
+      <h1>MyWallet</h1>
+      <form>
+        <input type={"email"} required></input>
+        <input type={"password"} required></input>
+        <button></button>
+      </form>
+      <Link to={"/signUp"}>Primeira vez? Cadastre-se!</Link>
     </Container>
   );
 }
 
-const Container = styled.div`
+export const Container = styled.div`
   min-height: 100vh;
   min-width: 100vw;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   background-color: #8c11be;
+
+  h1 {
+    font-family: "Saira Stencil One";
+    font-size: 32px;
+    line-height: 50px;
+    color: #ffff;
+  }
 `;
---
+++ src/SignUpPage.js
@@ -0,0 +1,9 @@
+import { Container } from "./LoginPage";
+
+export default function SignUpPage() {
+    return(
+        <Container>
+            <h1>MyWallet</h1>
+        </Container>
+    )
+}
